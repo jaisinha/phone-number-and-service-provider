151cfbfee739c325bb0affafce4b67111d166524
Jai Sinha <72357426+jaisinha@users.noreply.github.com>
2021-01-03 00:36:15 +0530

    first commit

--- phone number and service provider/phone number and service provider.py
+++ phone number and service provider/phone number and service provider.py
@@ -9,3 +9,7 @@ print(geocoder.description_for_number(ch_nmber,"en"))
 from phonenumbers import carrier
 service_nmber=phonenumbers.parse(number,"RO")
 print(carrier.name_for_number(service_nmber,"en"))
+
+#make a new file in vsc or pycharm or any other python code creating platform and write
+
+#namae="phone number"
